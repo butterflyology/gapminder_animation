179ce7ebc1051aa0a66647fc20dbd04792a54acf
Chris Hamm <topher.hamm@gmail.com>
2019-11-10 15:02:24 -0600

    .Rmd doc tutorial begun

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ I want to teach `gganimate` in November so I am recreating some iconic visualiza
 
 Commits:
 
-1. 2019-10-21: Initial commit. Got the Rosling plot for `gapminder` pretty close (needs larger points). Also the `diamonds` plot from Pederson. 
+1. 2019-10-21: Initial commit. Got the Rosling plot for `gapminder` pretty close (needs larger points). Also the `diamonds` plot from Pederson.
+1. 2019-11-10: Creating the `.Rmd` file with the tutorial.
--- code/gganimated.R
+++ code/gganimated.R
@@ -27,6 +27,7 @@ gap_plot <- gapminder %>%
   labs(y = "GDP PerCap", x = "Life Expectancy") +
   scale_y_continuous(breaks = seq(45, 85, 5), labels = as.character(seq(45, 85, 5))) +
   scale_x_continuous(breaks = c(1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000), labels = c("1000", "2000", "4000", "8000", "16K", "32K", "64K", "128K")) +
+  coord_cartesian(x = )
   theme(panel.grid = element_blank(),
         panel.ontop = TRUE,
         legend.position = "none")
@@ -82,3 +83,111 @@ diamond_animated <- animate(diamond_1)
 
 diamond_animated
 # anim_save(filename = "output/diamonds.gif", animation = diamond_animated)
+
+
+# more gapminder ----
+p <- gapminder %>% 
+  ggplot(
+    aes(x = gdpPercap, y = lifeExp, size = pop, color = country)) +
+  geom_point(show.legend = FALSE, alpha = 0.7) +
+  scale_color_viridis_d() +
+  scale_size(range = c(2, 12)) +
+  scale_x_log10() +
+  labs(x = "GDP per capita", y = "Life expectency")
+
+p + transition_time(year) +
+  labs(title = "Year: {frame_time}")
+
+p + facet_wrap(~continent) +
+  transition_time(year) + 
+  labs(title = "Year: {frame_time}")
+
+p + transition_time(year) +
+  labs(title = "Year: {frame_time}") +
+  view_follow(fixed_y = TRUE)
+
+p + transition_time(year) +
+  labs(title = "Year: {frame_time}") +
+  shadow_wake(wake_length = 0.1, alpha = TRUE)
+
+
+# airquality ----
+data(airquality)
+glimpse(airquality)
+range(airquality$Month)
+
+t <- airquality %>% 
+  ggplot(
+    aes(x = Day, y = Temp, group = Month, color = factor(Month))
+  ) +
+  geom_line() +
+  scale_color_viridis_d() +
+  labs(x = "Day of Month", y = "Temperature") +
+  theme(legend.position = "top")
+t
+
+t + transition_reveal(Day)
+
+t + 
+  geom_point() +
+  transition_reveal(Day)
+
+
+t2 <- airquality %>% 
+  ggplot(
+    aes(x = Day, y = Temp, group = Month, color = factor(Month))
+  ) +
+  geom_point() +
+  scale_color_viridis_d() +
+  labs(x = "Day of Month", y = "Temperature") +
+  theme(legend.position = "top")
+t
+
+t + transition_reveal(Day) +
+  shadow_wake(wake_length = 1, alpha = TRUE, size = TRUE)
+
+t + geom_point(aes(group = seq_along(Day))) +
+  transition_reveal(Day)
+
+mean_temp <- airquality %>% 
+  group_by(Month) %>% 
+  summarize(mTemp = mean(Temp))
+mean_temp
+
+t3 <- mean_temp %>% 
+  ggplot(
+    aes(x = Month, y = mTemp, fill = mTemp)) +
+  geom_col() +
+  scale_fill_distiller(palette = "Reds", direction = 1) +
+  theme_minimal() +
+  theme(
+    panel.grid = element_blank(),
+    panel.grid.major.y = element_line(color = "white"),
+    panel.ontop = TRUE)
+
+t3
+
+t3 + transition_states(Month, wrap = FALSE)
+
+t3 + transition_states(Month, wrap = FALSE) +
+  shadow_mark() +
+  enter_grow() + 
+  enter_fade()
+
+# anim_save()
+
+
+# boxplots ----
+range(mtcars$gear)
+
+mtcars %>% 
+  ggplot(aes(x = factor(cyl), y = mpg)) +
+  geom_boxplot() +
+  transition_states(
+    gear, 
+    transition_length = 2,
+    state_length = 1) +
+  enter_fade() +
+  exit_shrink() + 
+  ease_aes('sine-in-out') +
+  labs(title = "Gears: {closest_state}")
